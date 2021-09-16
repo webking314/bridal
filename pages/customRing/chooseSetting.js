533479e384e215e089c1d4bf6a94cee9acd56d34
webking530 <webking530@gmail.com>
2021-09-16 16:37:30 +1000

    complete bespoke page

--- pages/customRing/chooseSetting.js
+++ pages/customRing/chooseSetting.js
@@ -9,7 +9,12 @@ import SelectSearch, { fuzzySearch } from "react-select-search-nextjs";
 import Range from "../../components/range";
 import { useRouter } from "next/router";
 
-import { RiHeartLine, RiHeartFill } from "react-icons/ri";
+import {
+  RiHeartLine,
+  RiHeartFill,
+  RiFilter3Fill,
+  RiArrowRightSLine,
+} from "react-icons/ri";
 import { HiOutlineArrowLeft } from "react-icons/hi";
 import { filter } from "dom-helpers";
 const options = [
@@ -214,8 +219,8 @@ export default function ChooseSetting() {
       </Head>
       <Header />
       {/* Start hero section */}
-      <div className="state-section r-container">
-        <div className="link-panel py-3 mb-md-5 mb-0 d-flex align-items-center">
+      <div className="state-section">
+        <div className="link-panel r-container py-3 mb-md-5 mb-0 d-sm-flex d-none align-items-center">
           <button
             className="btn back-arrow d-flex me-3 blue-text px-0"
             onClick={() => router.back()}
@@ -238,164 +243,235 @@ export default function ChooseSetting() {
             CHOOSE A SETTING
           </span>
         </div>
-        <div className="setting-state-panel py-3 row m-0">
-          <div className="px-5 py-md-0 py-5 my-md-5 my-0 col-md-4 col-12 setting-state d-flex justify-content-between align-items-center active">
-            <div className="text-panel d-flex align-items-center">
-              <div className="number me-3 d-flex justify-content-center align-items-center">
-                1
-              </div>
-              <div className="title text-uppercase">
-                <p className="mb-1">Choose a</p>
-                <h3 className="m-0">Setting</h3>
-              </div>
-            </div>
-            <img
-              src="/img/customRing/chooseSetting/choose.png"
-              width="52"
-              height="52"
-              alt="state-image"
-            />
-          </div>
-          <div className="px-5 py-md-0 py-5 my-md-5 my-0 col-md-4 col-12 setting-state d-flex justify-content-between align-items-center">
-            <div className="text-panel d-flex align-items-center">
-              <div className="number me-3 d-flex justify-content-center align-items-center">
-                2
-              </div>
-              <div className="title text-uppercase">
-                <p className="mb-1">Choose a</p>
-                <h3 className="m-0">Diamond</h3>
-              </div>
-            </div>
-            <img
-              src="/img/customRing/chooseSetting/diamond.png"
-              width="52"
-              height="52"
-              alt="state-image"
-            />
-          </div>
-          <div className="px-5 py-md-0 py-5 my-md-5 my-0 col-md-4 col-12 setting-state d-flex justify-content-between align-items-center">
-            <div className="text-panel d-flex align-items-center">
-              <div className="number me-3 d-flex justify-content-center align-items-center">
-                3
-              </div>
-              <div className="title text-uppercase">
-                <p className="mb-1">Choose a</p>
-                <h3 className="m-0">Ring</h3>
-              </div>
-            </div>
-            <img
-              src="/img/customRing/chooseSetting/complete.png"
-              width="52"
-              height="52"
-              alt="state-image"
-            />
-          </div>
+        <div className="link-panel r-container py-3 mb-md-5 mb-0 d-sm-none d-flex align-items-center">
+          <button
+            className="btn back-arrow d-flex me-3 blue-text px-0"
+            onClick={() => router.back()}
+          >
+            <HiOutlineArrowLeft />
+          </button>
+          ...
+          <Link href="#">
+            <a className="mx-2">MAKE A RING</a>
+          </Link>
+          /
+          <span className="title ms-2 text-uppercase blue-text">
+            CHOOSE A SETTING
+          </span>
         </div>
-        <div className="ring-types d-flex justify-content-between align-items-center flex-wrap py-5">
-          {filterItems.map((item, index) => {
-            return (
-              <button
-                className="btn filter-item round-form mt-3"
-                key={index}
-                onClick={(event) => ringTypeHandle(event, index)}
-              >
-                <div className="image-panel text-center mb-3">
-                  <img
-                    src={"/img/customRing/chooseSetting/" + item.img}
-                    alt="filter-image"
-                  />
+
+        <div className="setting-state-panel">
+          <div className="r-container  py-md-3  row">
+            <div className="px-5 py-md-0 py-5 my-md-5 my-0 col-4 setting-state d-flex justify-content-between align-items-center active">
+              <div className="text-panel d-flex align-items-center">
+                <div className="number me-3 d-flex justify-content-center align-items-center">
+                  1
                 </div>
-                <h3 className="blue-text text-uppercase">{item.text}</h3>
-              </button>
-            );
-          })}
+                <div className="title text-uppercase">
+                  <p className="mb-1 d-md-block d-none">Choose a</p>
+                  <h3 className="m-0">Setting</h3>
+                </div>
+              </div>
+              <img
+                className="d-md-block d-none"
+                src="/img/customRing/chooseSetting/choose.png"
+                width="52"
+                height="52"
+                alt="state-image"
+              />
+            </div>
+            <div className="px-5 py-md-0 py-5 my-md-5 my-0 col-4 setting-state d-flex justify-content-between align-items-center">
+              <div className="text-panel d-flex align-items-center">
+                <div className="number me-3 d-flex justify-content-center align-items-center">
+                  2
+                </div>
+                <div className="title text-uppercase">
+                  <p className="mb-1 d-md-block d-none">Choose a</p>
+                  <h3 className="m-0">Diamond</h3>
+                </div>
+              </div>
+              <img
+                className="d-md-block d-none"
+                src="/img/customRing/chooseSetting/diamond.png"
+                width="52"
+                height="52"
+                alt="state-image"
+              />
+            </div>
+            <div className="px-5 py-md-0 py-5 my-md-5 my-0 col-4 setting-state d-flex justify-content-between align-items-center">
+              <div className="text-panel d-flex align-items-center">
+                <div className="number me-3 d-flex justify-content-center align-items-center">
+                  3
+                </div>
+                <div className="title text-uppercase">
+                  <p className="mb-1 d-md-block d-none">Choose a</p>
+                  <h3 className="m-0">Ring</h3>
+                </div>
+              </div>
+              <img
+                className="d-md-block d-none"
+                src="/img/customRing/chooseSetting/complete.png"
+                width="52"
+                height="52"
+                alt="state-image"
+              />
+            </div>
+          </div>
         </div>
-        <div className="setting-karat row m-0">
-          <div className="karat-panel col-lg-8 col-12 pe-5">
-            <h3 className="title text-uppercase pb-3">material</h3>
-            <div className="d-flex karat-list flex-wrap py-4 justify-content-between m-0">
-              {karats.map((item, index) => {
-                return (
-                  <button className="btn p-0 karat-item" key={index} onClick={(event) => karatItemHandle(event, index)}>
-                    <div
-                      className={
-                        "round-form karat px-lg-4 px-5 py-2 " + item.key
-                      }
+        <div className="r-container d-sm-block d-none">
+          <div className="ring-types d-flex justify-content-between align-items-center flex-wrap py-5">
+            {filterItems.map((item, index) => {
+              return (
+                <button
+                  className="btn filter-item round-form mt-3"
+                  key={index}
+                  onClick={(event) => ringTypeHandle(event, index)}
+                >
+                  <div className="image-panel text-center mb-3">
+                    <img
+                      src={"/img/customRing/chooseSetting/" + item.img}
+                      alt="filter-image"
+                    />
+                  </div>
+                  <h3 className="blue-text text-uppercase">{item.text}</h3>
+                </button>
+              );
+            })}
+          </div>
+          <div className="setting-karat row m-0">
+            <div className="karat-panel col-lg-8 col-12 pe-5">
+              <h3 className="title text-uppercase pb-3">material</h3>
+              <div className="d-flex karat-list flex-wrap py-4 justify-content-between m-0">
+                {karats.map((item, index) => {
+                  return (
+                    <button
+                      className="btn p-0 karat-item"
+                      key={index}
+                      onClick={(event) => karatItemHandle(event, index)}
                     >
-                      {item.karat}
-                    </div>
-                    <p className="pt-2">{item.title}</p>
-                  </button>
-                );
-              })}
+                      <div
+                        className={
+                          "round-form karat px-lg-4 px-5 py-2 " + item.key
+                        }
+                      >
+                        {item.karat}
+                      </div>
+                      <p className="pt-2">{item.title}</p>
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="cost-panel col-lg-4 col-12 px-5">
+              <h3 className="title text-uppercase blue-text m-0 pb-4">Cost</h3>
+              <div className="range-panel d-flex align-items-center mt-4">
+                <div className="range-min-pointer range-pointer" />
+                <div className="range-max-pointer range-pointer" />
+                <Range min={0} max={109000} unit={"$"} />
+              </div>
             </div>
           </div>
-          <div className="cost-panel col-lg-4 col-12 px-5">
-            <h3 className="title text-uppercase blue-text m-0 pb-4">Cost</h3>
-            <div className="range-panel d-flex align-items-center mt-4">
-              <div className="range-min-pointer range-pointer" />
-              <div className="range-max-pointer range-pointer" />
-              <Range min={0} max={109000} unit={"$"} />
-            </div>
+        </div>
+        <div className="r-container mobile-setting-bar d-sm-none d-block">
+          <div className="title-panel mt-5">
+            <h3 className="blue-text">Choose Ring</h3>
+            <p className="text-uppercase">
+              Engagement Rings (Solitaire) (3455 items)
+            </p>
           </div>
+          <button className="btn d-flex btn-filter round-form justify-content-between align-items-center px-5 py-3">
+            <div className="text-uppercase d-flex align-items-center">
+              <RiFilter3Fill className="me-3" />
+              Filter
+            </div>
+            <RiArrowRightSLine />
+          </button>
         </div>
       </div>
       {/* End Hero section */}
 
       {/* Start product section */}
       <div className="product-section r-container py-5 ">
-        {products.map((item, index) => {
-          if (item.url)
-            return (
-              <div className="mb-5 p-0 product-item" key={index}>
-                <Link href={item.url}>
-                  <a>
-                    <div className="product-image d-flex justify-content-center align-items-center round">
-                      <img
-                        src={"/img/customRing/chooseSetting/" + item.img}
-                        alt="product-image"
-                      />
-                    </div>
-                    <h3 className="text-uppercase blue-text py-4 m-0">
-                      {item.title}
-                    </h3>
-                    <p className="text-uppercase m-0">
-                      {item.categories.map((category, key) => {
-                        return (
-                          <span key={key} className="me-2">
-                            {category}
-                          </span>
-                        );
-                      })}
-                    </p>
-                    <h4 className="blue-text">{item.cost}</h4>
-                  </a>
-                </Link>
-                <div className="color-panel py-4 mb-4">
-                  <button className="btn white me-3"></button>
-                  <button className="btn yellow me-3"></button>
-                  <button className="btn platinium me-3"></button>
-                  <button className="btn rose-gold me-3"></button>
+        <div className="top-bar row align-items-center m-0 py-3">
+          <div className="title-panel col-md-6 col-12 p-0 pb-md-0 pb-3">
+            <h2>Choose Diamond</h2>
+            <p className="text-uppercase">
+              Certified Diamonds ({result} items)
+            </p>
+          </div>
+          <div className="col-md-6 col-12 d-flex justify-content-end p-0 pt-3 pt-md-0">
+            <div className="search-box round-form d-flex align-items-center">
+              <label htmlFor="selectSearch" className="px-4">
+                SORT BY :{" "}
+              </label>
+              <SelectSearch
+                id="selectSearch"
+                options={options}
+                value={selectValue}
+                onChange={(value) => {
+                  setSelectValue(value);
+                }}
+                filterOptions={fuzzySearch}
+                emptyMessage="Not found"
+                search
+              />
+            </div>
+          </div>
+        </div>
+        <div className="main-panel py-5">
+          {products.map((item, index) => {
+            if (item.url)
+              return (
+                <div className="mb-5 p-0 product-item" key={index}>
+                  <Link href={item.url}>
+                    <a>
+                      <div className="product-image d-flex justify-content-center align-items-center round">
+                        <img
+                          src={"/img/customRing/chooseSetting/" + item.img}
+                          alt="product-image"
+                        />
+                      </div>
+                      <h3 className="text-uppercase blue-text py-4 m-0">
+                        {item.title}
+                      </h3>
+                      <p className="text-uppercase m-0">
+                        {item.categories.map((category, key) => {
+                          return (
+                            <span key={key} className="me-2">
+                              {category}
+                            </span>
+                          );
+                        })}
+                      </p>
+                      <h4 className="blue-text">{item.cost}</h4>
+                    </a>
+                  </Link>
+                  <div className="color-panel py-4 mb-4">
+                    <button className="btn white me-3"></button>
+                    <button className="btn yellow me-3"></button>
+                    <button className="btn platinium me-3"></button>
+                    <button className="btn rose-gold me-3"></button>
+                  </div>
+                  <div className="favor-icon " onClick={setFavor}>
+                    <RiHeartLine className="unfavor" />
+                    <RiHeartFill className="favor" />
+                  </div>
                 </div>
-                <div className="favor-icon " onClick={setFavor}>
-                  <RiHeartLine className="unfavor" />
-                  <RiHeartFill className="favor" />
+              );
+            else
+              return (
+                <div className="image-panel round" key={index}>
+                  <img src={"/img/customRing/chooseSetting/" + item.img} />
                 </div>
-              </div>
-            );
-          else
-            return (
-              <div className="image-panel round" key={index}>
-                <img src={"/img/customRing/chooseSetting/" + item.img} />
-              </div>
-            );
-        })}
-        <button
-          className="btn load-more-btn text-uppercase mt-5 py-3 px-5 round-form"
-          onClick={loadMore}
-        >
-          Load More
-        </button>
+              );
+          })}
+          <button
+            className="btn load-more-btn text-uppercase mt-5 py-3 px-5 round-form"
+            onClick={loadMore}
+          >
+            Load More
+          </button>
+        </div>
       </div>
       {/* End product section */}
 
