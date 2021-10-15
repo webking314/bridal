79e118e56fb3a6dcab97953f7d6467f237a46be4
webking530 <webking530@gmail.com>
2021-10-16 04:16:06 +1000

    -update instagram layout

--- components/instagram.js
+++ components/instagram.js
@@ -1,18 +1,30 @@
 import Link from "next/link";
+import { useState, useEffect } from "react";
 import AboutSlider from "./aboutSlider";
-import InstagramFeed  from 'react-ig-feed'
+import InstagramFeed from 'react-ig-feed'
 import 'react-ig-feed/dist/index.css'
+import SwiperCore, { Autoplay, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
-const instagramItems = [
-  { img: "/img/homepage/Rectangle 34.png" },
-  { img: "/img/homepage/Rectangle 38.png" },
-  { img: "/img/homepage/Rectangle 37.png" },
-  { img: "/img/homepage/Rectangle 36.png" },
-  { img: "/img/homepage/Rectangle 35.png" },
-  { img: "/img/homepage/Rectangle 1.png" },
-];
+SwiperCore.use([Autoplay, Navigation]);
+
+const getInstagramURL = "https://graph.instagram.com/me/media?fields=media_count,media_type,permalink,media_url&&access_token=IGQVJYWjhoWlB5SGIzSU9NTndXODBya1pCZAWxWbGt1NmNQNUdHSnFrakoyd1hjUFJSZAHJDU1MxQ3BQOC1qWmM1X2s0TzZAockVFRm5fTUsyRTA5MDlkT3NNdURlNnNyWFFraUg2cUpVRE9zV2RvWmxSTQZDZD";
 
 export default function Instagram() {
+  const navigationPrevRef = React.useRef(null);
+  const navigationNextRef = React.useRef(null);
+  const [instagramData, setInstagramData] = useState();
+
+  useEffect(() => {
+    fetch(getInstagramURL, {
+      method: "get"
+    }).then(res => res.json())
+      .then(data => {
+        setInstagramData(data.data)
+      })
+  }, [])
+
   return (
     <div className="instagram pb-5">
       <div className="r-container">
@@ -25,126 +37,310 @@ export default function Instagram() {
           <div className="col-md-6 col-12 p-0 d-flex flex-column justify-content-end link-panel">
             <div className="mx-0 text-md-end text-start">
               Follow{" "}
-              <Link  passHref={true} href="#">
+              <Link passHref={true} href="#">
                 <a className="text-primary">#Royalcoster</a>
               </Link>{" "}
               @Instagram For
             </div>
             <div className="mx-0 text-md-end text-start">
-              <Link  passHref={true} href="#">
+              <Link passHref={true} href="#">
                 <a className="text-primary">#Diamondstories</a>
               </Link>
               ,{" "}
-              <Link  passHref={true} href="#">
+              <Link passHref={true} href="#">
                 <a className="text-primary">#Inspiration</a>
               </Link>{" "}
               &{" "}
-              <Link  passHref={true} href="#">
+              <Link passHref={true} href="#">
                 <a className="text-primary">#Amsterdiamonds</a>
               </Link>
             </div>
           </div>
         </div>
-        <InstagramFeed token="IGQVJYWjhoWlB5SGIzSU9NTndXODBya1pCZAWxWbGt1NmNQNUdHSnFrakoyd1hjUFJSZAHJDU1MxQ3BQOC1qWmM1X2s0TzZAockVFRm5fTUsyRTA5MDlkT3NNdURlNnNyWFFraUg2cUpVRE9zV2RvWmxSTQZDZD"  counter="8"/>
-        {/* <div className="d-md-none d-block instagram-slider-panel">
-          <AboutSlider componentProduct={true} slides={instagramItems} />
-        </div>
-        <div className="row gallery-panel d-md-flex d-none m-0 p-0">
-          <div className="col-md-6 col-12 m-0 p-0 d-flex flex-column justify-content-between">
-            <div className="row m-0 p-0">
-              <div className="col-6 d-flex m-0 p-0">
-                <Link  passHref={true} href="#">
-                  <a className="instagram-link gallery-type-1">
-                    <div className="gallery-item round">
-                      <img
-                        src="/img/homepage/Rectangle 34.png"
-                        className="round"
-                        alt="gallery-img"
-                      />
-                      <div className="hover-panel"></div>
-                    </div>
-                  </a>
-                </Link>
-              </div>
-              <div className="col-6 d-flex m-0 p-0">
-                <Link  passHref={true} href="#">
-                  <a className="instagram-link gallery-type-1">
-                    <div className="gallery-item round">
-                      <img
-                        src="/img/homepage/Rectangle 38.png"
-                        className="round"
-                        alt="gallery-img"
-                      />
-                      <div className="hover-panel"></div>
-                    </div>
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div className="row m-0 p-0">
-              <Link  passHref={true} href="#">
-                <a className="instagram-link p-0 gallery-type-1">
-                  <div className="gallery-item round">
-                    <img
-                      src="/img/homepage/Rectangle 37.png"
-                      className="round"
-                      alt="gallery-img"
-                    />
-                    <div className="hover-panel"></div>
-                  </div>
-                </a>
-              </Link>
-            </div>
-          </div>
-          <div className="col-md-6 col-12 p-0">
-            <div className="row m-0 p-0">
-              <div className="col-6 p-0">
-                <div className="row m-0 p-0">
-                  <Link  passHref={true} href="#">
-                    <a className="instagram-link p-0 gallery-type-1">
-                      <div className="gallery-item round">
-                        <img
-                          src="/img/homepage/Rectangle 36.png"
-                          className="round"
-                          alt="gallery-img"
-                        />
-                        <div className="hover-panel"></div>
-                      </div>
-                    </a>
-                  </Link>
-                </div>
-                <div className="row m-0 p-0">
-                  <Link  passHref={true} href="#">
-                    <a className="instagram-link p-0 gallery-type-1">
-                      <div className="gallery-item round">
-                        <img
-                          src="/img/homepage/Rectangle 35.png"
-                          className="round"
-                          alt="gallery-img"
-                        />
-                        <div className="hover-panel"></div>
-                      </div>
-                    </a>
-                  </Link>
-                </div>
-              </div>
-              <div className="col-6 d-flex p-0">
-                <Link  passHref={true} href="#">
-                  <a className="instagram-link gallery-type-2">
-                    <div className="gallery-item round">
-                      <img
-                        src="/img/homepage/Rectangle 1.png"
-                        className="round"
-                        alt="gallery-img"
-                      />
-                      <div className="hover-panel"></div>
-                    </div>
-                  </a>
-                </Link>
-              </div>
-            </div>
-          </div>
+        {/* <div className="instagram-feed-panel">
+          <InstagramFeed token="IGQVJYWjhoWlB5SGIzSU9NTndXODBya1pCZAWxWbGt1NmNQNUdHSnFrakoyd1hjUFJSZAHJDU1MxQ3BQOC1qWmM1X2s0TzZAockVFRm5fTUsyRTA5MDlkT3NNdURlNnNyWFFraUg2cUpVRE9zV2RvWmxSTQZDZD" counter="8" />
         </div> */}
+        <div className="d-md-none d-block instagram-slider-panel">
+          <Swiper
+            navigation={{
+              prevEl: navigationPrevRef.current,
+              nextEl: navigationNextRef.current,
+            }}
+            slidesPerView={3}
+            spaceBetween={30}
+            loop={true}
+            className="about-slider"
+            breakpoints={{
+              1024: {
+                slidesPerView: 3,
+              },
+              990: {
+                slidesPerView: 2.8,
+              },
+              768: {
+                slidesPerView: 2.4,
+              },
+              480: {
+                slidesPerView: 1,
+              },
+              1: {
+                slidesPerView: 1,
+              },
+            }}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            onSwiper={(swiper) => {
+              // Delay execution for the refs to be defined
+              setTimeout(() => {
+                // Override prevEl & nextEl now that refs are defined
+                swiper.params.navigation.prevEl = navigationPrevRef.current;
+                swiper.params.navigation.nextEl = navigationNextRef.current;
+
+                // Re-init navigation
+                swiper.navigation.destroy();
+                swiper.navigation.init();
+                swiper.navigation.update();
+              });
+            }}
+          >
+            {instagramData && instagramData.map((item, index) => {
+              return (
+                <SwiperSlide key={index}>
+                  <Link passHref={true} href={item.permalink}>
+                    <a>
+                      <div className="image-panel hover-scale round">
+                        {
+                          item.media_type == "IMAGE"
+                            ? < img
+                              src={item.media_url}
+                              className="round"
+                              alt="gallery-img"
+                            />
+                            : <video
+                              autoPlay="autoplay"
+                              loop="loop"
+                              muted
+                              defaultmuted="defaultmuted"
+                              playsInline
+                              onContextMenu={() => false}
+                              preload="auto"
+                              className="bg_video"
+                            >
+                              <source src={item.media_url} type="video/mp4" />
+                            </video>
+                        }
+                      </div>
+                    </a>
+                  </Link>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+          <div
+            className={
+              "btn-bottom-panel mt-sm-5 mt-0 pt-5 " + (btnDisable ? "d-none" : "")
+            }
+          >
+            <button ref={navigationPrevRef} className="btn px-0 me-5">
+              <img src="/img/common/leftArrow_black.png" alt="rightArrow" />
+            </button>
+            <button ref={navigationNextRef} className="btn px-0">
+              <img src="/img/common/rightArrow_black.png" alt="rightArrow" />
+            </button>
+          </div>
+        </div>
+        {
+          instagramData &&
+          <div className="row gallery-panel d-md-flex d-none m-0 p-0">
+            <div className="col-md-6 col-12 m-0 p-0 d-flex flex-column justify-content-between">
+              <div className="row m-0 p-0">
+                <div className="col-6 d-flex m-0 p-0">
+                  <Link passHref={true} href={instagramData[0].permalink}>
+                    <a className="instagram-link gallery-type-1">
+                      <div className="gallery-item round">
+                        {
+                          instagramData[0].media_type == "IMAGE"
+                            ? < img
+                              src={instagramData[0].media_url}
+                              className="round"
+                              alt="gallery-img"
+                            />
+                            : <video
+                              autoPlay="autoplay"
+                              loop="loop"
+                              muted
+                              defaultmuted="defaultmuted"
+                              playsInline
+                              onContextMenu={() => false}
+                              preload="auto"
+                              className="bg_video"
+                            >
+                              <source src={instagramData[0].media_url} type="video/mp4" />
+                            </video>
+                        }
+                        <div className="hover-panel"></div>
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+                <div className="col-6 d-flex m-0 p-0">
+                  <Link passHref={true} href={instagramData[0].permalink}>
+                    <a className="instagram-link gallery-type-1">
+                      <div className="gallery-item round">
+                        {
+                          instagramData[1].media_type == "IMAGE"
+                            ? < img
+                              src={instagramData[1].media_url}
+                              className="round"
+                              alt="gallery-img"
+                            />
+                            : <video
+                              autoPlay="autoplay"
+                              loop="loop"
+                              muted
+                              defaultmuted="defaultmuted"
+                              playsInline
+                              onContextMenu={() => false}
+                              preload="auto"
+                              className="bg_video"
+                            >
+                              <source src={instagramData[1].media_url} type="video/mp4" />
+                            </video>
+                        }
+                        <div className="hover-panel"></div>
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+              <div className="row m-0 p-0">
+                <Link passHref={true} href={instagramData[2].permalink}>
+                  <a className="instagram-link p-0 gallery-type-1">
+                    <div className="gallery-item round">
+                      {
+                        instagramData[2].media_type == "IMAGE"
+                          ? < img
+                            src={instagramData[2].media_url}
+                            className="round"
+                            alt="gallery-img"
+                          />
+                          : <video
+                            autoPlay="autoplay"
+                            loop="loop"
+                            muted
+                            defaultmuted="defaultmuted"
+                            playsInline
+                            onContextMenu={() => false}
+                            preload="auto"
+                            className="bg_video"
+                          >
+                            <source src={instagramData[2].media_url} type="video/mp4" />
+                          </video>
+                      }
+                      <div className="hover-panel"></div>
+                    </div>
+                  </a>
+                </Link>
+              </div>
+            </div>
+            <div className="col-md-6 col-12 p-0">
+              <div className="row m-0 p-0">
+                <div className="col-6 p-0">
+                  <div className="row m-0 p-0">
+                    <Link passHref={true} href={instagramData[3].permalink}>
+                      <a className="instagram-link p-0 gallery-type-1">
+                        <div className="gallery-item round">
+                          {
+                            instagramData[3].media_type == "IMAGE"
+                              ? < img
+                                src={instagramData[3].media_url}
+                                className="round"
+                                alt="gallery-img"
+                              />
+                              : <video
+                                autoPlay="autoplay"
+                                loop="loop"
+                                muted
+                                defaultmuted="defaultmuted"
+                                playsInline
+                                onContextMenu={() => false}
+                                preload="auto"
+                                className="bg_video"
+                              >
+                                <source src={instagramData[3].media_url} type="video/mp4" />
+                              </video>
+                          }
+                          <div className="hover-panel"></div>
+                        </div>
+                      </a>
+                    </Link>
+                  </div>
+                  <div className="row m-0 p-0">
+                    <Link passHref={true} href={instagramData[4].permalink}>
+                      <a className="instagram-link p-0 gallery-type-1">
+                        <div className="gallery-item round">
+                          {
+                            instagramData[4].media_type == "IMAGE"
+                              ? < img
+                                src={instagramData[4].media_url}
+                                className="round"
+                                alt="gallery-img"
+                              />
+                              : <video
+                                autoPlay="autoplay"
+                                loop="loop"
+                                muted
+                                defaultmuted="defaultmuted"
+                                playsInline
+                                onContextMenu={() => false}
+                                preload="auto"
+                                className="bg_video"
+                              >
+                                <source src={instagramData[4].media_url} type="video/mp4" />
+                              </video>
+                          }
+                          <div className="hover-panel"></div>
+                        </div>
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+                <div className="col-6 d-flex p-0">
+                  <Link passHref={true} href={instagramData[5].permalink}>
+                    <a className="instagram-link gallery-type-2">
+                      <div className="gallery-item round">
+                        {
+                          instagramData[5].media_type == "IMAGE"
+                            ? < img
+                              src={instagramData[5].media_url}
+                              className="round"
+                              alt="gallery-img"
+                            />
+                            : <video
+                              autoPlay="autoplay"
+                              loop="loop"
+                              muted
+                              defaultmuted="defaultmuted"
+                              playsInline
+                              onContextMenu={() => false}
+                              preload="auto"
+                              className="bg_video"
+                            >
+                              <source src={instagramData[5].media_url} type="video/mp4" />
+                            </video>
+                        }
+                        <div className="hover-panel"></div>
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        }
         <button className="btn round-form mt-5 px-5 py-3 follow-btn blue-outline-btn">
           Follow @Costerdiamondsofficial
         </button>
