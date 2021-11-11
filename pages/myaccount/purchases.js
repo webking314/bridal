ffeb4c9fe66417a02671cba29d96cff81a8a4aff
webking530 <webking530@gmail.com>
2021-11-11 18:01:16 +1000

    -update my account page

--- pages/myaccount/purchases.js
+++ pages/myaccount/purchases.js
@@ -25,15 +25,15 @@ export default function Purchases() {
       <div className="list-panel">
         <div className="item-panel row m-0 mb-3">
           <div className="title-panel d-md-block d-sm-flex  d-block col-md-3 p-0">
-            <div className="order-status-panel mb-md-3 md-sm-0 md-3 me-md-0 me-sm-4 me-0">
+            <div className="order-status-panel mb-md-3 mb-0 me-md-0 me-3">
               <h3>Order Status</h3>
               <p>Sent</p>
             </div>
-            <div className="order-number-panel mb-md-3 md-sm-0 md-3 me-md-0 me-sm-4 me-0">
+            <div className="order-number-panel mb-md-3 mb-0 me-md-0 me-3">
               <h3>Order Number</h3>
               <p>EU036174534</p>
             </div>
-            <div className="order-number-panel mb-md-3 md-sm-0 md-3 me-md-0 me-sm-4 me-0">
+            <div className="order-number-panel mb-md-3 mb-0">
               <h3>Order Date</h3>
               <p>10/8/21</p>
             </div>
@@ -59,8 +59,8 @@ export default function Purchases() {
                 />
               </div>
             </div>
-            <div className="btn-panel d-flex align-items-center">
-              <button className="btn  btn-order-detail blue-btn round-form text-uppercase me-2">
+            <div className="btn-panel d-sm-flex d-block align-items-center">
+              <button className="btn btn-order-detail mb-sm-0 mb-4 blue-btn round-form text-uppercase me-2">
                 order details
               </button>
               <button className="btn btn-reorder round-form text-uppercase">
